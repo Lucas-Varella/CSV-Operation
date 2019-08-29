1bc235264dc0301a85b5875c36e5510e466ac1f1
Usuario <Usuario@BigBadWolf>
2019-08-29 13:11:52 -0300

    Print-step completed: header, highest price, types, lowest generic price

--- CSV-Operation/src/Main.java
+++ CSV-Operation/src/Main.java
@@ -4,6 +4,8 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -11,45 +13,67 @@ public class Main {
 		String inputPath = System.getProperty("user.dir")+"/src/Input/TA_PRECO_MEDICAMENTO.csv";
 		String outputPath = System.getProperty("user.dir")+"/src/Input/output.csv";
 		String line = "";
-		ArrayList<String> table = new ArrayList<String>();
 		double maxPf = 0;
-		ArrayList<Double> maxPfs = new ArrayList<Double>();
+		double minPf = 0;
+		ArrayList<String> maxPfs = new ArrayList<String>();
+		ArrayList<String> minPfs = new ArrayList<String>();
+		ArrayList<String> productTypes = new ArrayList<String>();
+		
 		NumberFormat nf = NumberFormat.getInstance(Locale.FRANCE);
 		
 		
+		
+
 		try {
 			FileReader fReader = new FileReader(inputPath);
 			BufferedReader reader = new BufferedReader(fReader);
 			ArrayList<String> header = new ArrayList<String>(Arrays.asList(reader.readLine().split(";"))) ;
 			System.out.println(header);
-			int pfSemImpostosIndex = header.indexOf("PF Sem Impostos");
-			int count = 0;
 			while ((line = reader.readLine()) != null) {
-				count++;
 				String[] row = line.split(";");	
 				try {
 					Number pfN = nf.parse(row[13]);
 					Double pf = pfN.doubleValue();
 					if(pf > maxPf) {
 						maxPf = pf;
-						maxPfs = new ArrayList<Double>();
-						maxPfs.add(pf);
+						maxPfs = new ArrayList<String>();
+						maxPfs.add(row[5]);
 					}else if(pf == maxPf) {
-						maxPfs.add(pf);
+						maxPfs.add(row[5]);
+					}else if(row[11].equals("Genérico") && pf < minPf || minPf == 0) {
+						minPfs = new ArrayList<String>();
+						minPf = pf;
+						minPfs.add(row[5]);
+						
+					}else if (row[11].equals("Genérico") && pf == minPf ) {
+						minPfs.add(row[5]);
+					}
+					if(pf > 100 && row[38].equals("Sim") && row[39].equals("Tarja Vermelha")) {
+						
 					}
 				} catch (ParseException e1) {
 					
 				} catch (ArrayIndexOutOfBoundsException e2) {
 					
 				}
+				try {
+					if(productTypes.indexOf(row[11])==-1) {
+						productTypes.add(row[11]);
+					}				
+				} catch (ArrayIndexOutOfBoundsException e2) {
+					
+				}
+
 			}
 			
 			
+			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			
 		}
 		System.out.println(maxPfs);
+		System.out.println(productTypes);
+		System.out.println(minPfs);
 		
 		
 	}
